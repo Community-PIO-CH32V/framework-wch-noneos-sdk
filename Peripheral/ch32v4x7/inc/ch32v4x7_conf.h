06b3e57ab58789d3decca48e8e3da2cd64bffe24
Zuyi Hu <hzy068808@gmail.com>
2026-06-14 12:02:52 +0800

    Add platformio porting for ch32v4x7

--- Peripheral/ch32v4x7/inc/ch32v4x7_conf.h
+++ Peripheral/ch32v4x7/inc/ch32v4x7_conf.h
@@ -42,12 +42,16 @@
 #include "ch32v4x7_usart.h"
 #include "ch32v4x7_wwdg.h"
 #include "ch32v4x7_usb.h"
-#include "ch32v4x7_it.h"
 
+/* RT Thread expects the interrupt header file from the project to be included */
+/* Support both our unified name and the old name */
+#if defined(__PIO_BUILD_RT_THREAD__)
+#if __has_include("ch32v_it.h")
+# include "ch32v_it.h"
+#elif __has_include("ch32v4x7_it.h")
+# include "ch32v4x7_it.h"
+#endif
+#endif
 
 #endif /* __CH32V4x7_CONF_H */
 
-
-	
-	
-	
